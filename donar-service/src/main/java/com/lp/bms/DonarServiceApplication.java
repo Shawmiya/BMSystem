aafe79b2b5379e7f76ac672f13559a0fdc033d6b
Shawmiya <yoshawmi94@gmail.com>
2020-03-11 06:57:38 +0530

    added discovery service and donar service

---
+++ donar-service/src/main/java/com/lp/bms/DonarServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lp.bms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonarServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonarServiceApplication.class, args);
+	}
+
+}
---
+++ donar-service/src/test/java/com/lp/bms/DonarServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lp.bms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonarServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
